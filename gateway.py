3630ca6adf78ddf00360c94d5c5427744d5307ad
Matheus Lessa <matheus.220@hotmail.com>
2019-11-23 01:58:56 -0300

    Remove unused code

--- gateway.py
+++ gateway.py
@@ -23,8 +23,6 @@ logging.basicConfig()
 SENSORS = {}
 ADDR_ID_MAP = {}
 
-USERS = set()
-
 PORT = 80
 Handler = http.server.SimpleHTTPRequestHandler
 
@@ -36,42 +34,6 @@ multicast_port = 20480
 
 CREDENTIALS = pika.PlainCredentials('projetosd', 'projetosd')
 RABBITMQ_HOST = socket.gethostbyname(socket.gethostname())
-
-
-def state_event():
-    return build_message()
-
-
-def users_event():
-    return json.dumps({"type": "users", "count": len(USERS)})
-
-
-async def notify_state():
-    if USERS:  # asyncio.clswait doesn't accept an empty list
-        _ = sensor_pb2.Message()
-        await asyncio.wait([user.send(build_message().SerializeToString()) for user in USERS])
-
-
-async def notify_users():
-    if USERS:  # asyncio.wait doesn't accept an empty list
-        await asyncio.wait([user.send(build_message().SerializeToString()) for user in USERS])
-
-
-def build_message():
-    _ = sensor_pb2.Message()
-    for a,b in SENSORS:
-        serialize_obj(_.sensors.add(),SENSORS[(a,b)],a,b,SENSORS[(a,b)].get('data'))
-    return _
-
-
-async def register(websocket):
-    USERS.add(websocket)
-    await notify_users()
-
-
-async def unregister(websocket):
-    USERS.remove(websocket)
-    await notify_users()
 
 
 class Consumer(Thread):
@@ -159,7 +121,7 @@ class Consumer(Thread):
         print(" |--- Queue name: " + self.queue_name)
 
 
-async def connection_handler(websocket, path):
+async def ws_connection_handler(websocket, _):
     consumer = Consumer(websocket)
     consumer.start()
 
@@ -226,7 +188,6 @@ async def sensor_finder_handler():
                 print("Sensor " + SENSORS[key]['sensor_id'] + " removed")
                 del ADDR_ID_MAP[SENSORS[key]['sensor_id']]
                 del SENSORS[key]
-            await notify_state()
 
 
 def multicast_thread():
@@ -259,17 +220,17 @@ async def multicast_handler():
     # Receive/respond loop
     while True:
         data, address = sock.recvfrom(1024)
-        if(data == b'SENSOR'):
+        if data == b'SENSOR':
             sock.sendto('SERVER'.encode(), address)
         else:
             split = data.decode().split('_')
-            if(len(split) == 4 and split[0]=="SENSOR"):
+            if len(split) == 4 and split[0] == "SENSOR":
                 device_id = split[1]
                 sensor_type = split[2]
                 sensor_port = int(split[3])
                 sensor_ip = address[0]
                 sensor_id = device_id + "_" + sensor_type
-                if(sensor_id not in ADDR_ID_MAP.keys()):
+                if sensor_id not in ADDR_ID_MAP.keys():
                     ADDR_ID_MAP[sensor_id] = (sensor_ip, sensor_port)
                     SENSORS[(sensor_ip, sensor_port)] = {
                         'sensor_id': sensor_id,
@@ -282,60 +243,7 @@ async def multicast_handler():
                         del SENSORS[ADDR_ID_MAP[sensor_id]]
                         ADDR_ID_MAP[sensor_id] = (sensor_ip, sensor_port)
                     SENSORS[(sensor_ip, sensor_port)]['last_msg_date'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                await notify_state()
                 print('Sensor identified: {}'.format(sensor_id))
-
-
-def serialize_obj(obj,_,address,port,data):
-    typ = _['sensor_id'].split('_')[1]
-    obj.id = _['sensor_id']
-    obj.addr = address
-    obj.port = port
-    if data:
-        obj.data = json.dumps(data)
-    obj.last_msg_date = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    if typ == 'LIGHT':
-        obj.type =  sensor_pb2.Sensor.LIGHT
-    if typ == 'MAGNETIC':
-        obj.type = sensor_pb2.Sensor.MAGNETIC
-    if typ == 'SOUND':
-        obj.type = sensor_pb2.Sensor.SOUND
-    if typ == 'LED':
-        obj.type = sensor_pb2.Sensor.LED
-
-def data_listener_thread():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    print("Waiting for UDP messages")
-
-    loop.run_until_complete(udp_handler())
-    loop.close()
-
-
-async def udp_handler():
-    UDP_PORT_NO = 5003
-    BUFFER_SIZE = 1024
-
-    serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    serverSock.bind(("", UDP_PORT_NO))    
-
-    # Receive/respond loop
-    while True:
-        data, addr = serverSock.recvfrom(BUFFER_SIZE)
-        if addr in SENSORS.keys():
-            SENSORS[addr]['last_msg_date'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S");
-            if(SENSORS[addr]['type'] == 'LIGHT'):
-                SENSORS[addr]['data'] = float(data.decode("UTF-8"))
-            elif(SENSORS[addr]['type'] == 'MAGNETIC'):
-                split = data.decode("UTF-8").split(',')
-                SENSORS[addr]['data'] = {'x': "{0:.2f}".format(float(split[0])), 'y': "{0:.2f}".format(float(split[1])), 'z': "{0:.2f}".format(float(split[2]))}
-            elif(SENSORS[addr]['type'] == 'LED'):
-                SENSORS[addr]['data'] = data.decode("UTF-8")
-            elif(SENSORS[addr]['type'] == 'SOUND'):
-                split = data.decode("UTF-8").split(',')
-                SENSORS[addr]['data'] = {'status': split[0], 'volume': split[1]}
-            await notify_state()
-            print("New message from ", SENSORS[addr]['sensor_id'])
 
 
 if __name__ == '__main__':
@@ -345,11 +253,10 @@ if __name__ == '__main__':
         sys.stdout.write(line)
 
     Thread(target=multicast_thread, daemon=True).start()
-    Thread(target=data_listener_thread, daemon=True).start()
     Thread(target=http_server, daemon=True).start()
     Thread(target=sensor_finder_thread, daemon=True).start()
 
-    start_server = websockets.serve(connection_handler, "", 6789)
+    start_server = websockets.serve(ws_connection_handler, "", 6789)
 
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
